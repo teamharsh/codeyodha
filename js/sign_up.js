d97551224f75e821ab77df175e80d24467742cac
Sameer <contact.sameer04@gmail.com>
2023-08-16 21:12:31 +0530

    signUP_page_added

---
+++ js/sign_up.js
@@ -0,0 +1,57 @@
+function signup() {
+  var name = document.getElementById('name').value;
+  var branch = document.getElementById('branch').value;
+  var batch = document.getElementById('batch').value;
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  if (
+    name === '' ||
+    branch === '' ||
+    batch === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    alert('Please Fill the Form properly');
+  } else {
+    localStorage.setItem('name1', name);
+    localStorage.setItem('branch1', branch);
+    localStorage.setItem('batch1', batch);
+    localStorage.setItem('email1', email);
+    localStorage.setItem('password1', password);
+    openPopup();
+  }
+}
+
+let popup = document.getElementById('popup');
+
+function openPopup() {
+  popup.classList.add('open-popup');
+}
+
+function closePopup() {
+  popup.classList.remove('open-popup');
+}
+
+//Main JS for final page
+// function signup() {
+//   var name = document.getElementById('name').value;
+//   var branch = document.getElementById('branch').value;
+//   var batch = document.getElementById('batch').value;
+//   var email = document.getElementById('email').value;
+//   var password = document.getElementById('password').value;
+
+//   localStorage.setItem('name1', name);
+//   localStorage.setItem('branch1', branch);
+//   localStorage.setItem('batch1', batch);
+//   localStorage.setItem('email1', email);
+//   localStorage.setItem('password1', password);
+
+//   openPopup();
+// }
+
+// let popup = document.getElementById('popup');
+
+// function openPopup() {
+//   popup.classList.add('open-popup');
+// }
